5b1183c928f3ff842bca143a45e67dac822495a0
Svyatoslav Gryaznov <nightuser@ya.ru>
2015-03-06 21:48:37 +0300

    Refactor parser to anonymous parser

--- parser.ml
+++ parser.ml
@@ -1,20 +1,21 @@
 (* dummy parser *)
 
 let rec parse_expr = parser
-  | [< lhs = parse_summand; stream >] ->
-    (parser
-      | [< 'Token.Plus; rhs = parse_expr >] -> Ast.Plus(lhs, rhs)
-      | [< 'Token.Minus; rhs = parse_expr >] -> Ast.Minus(lhs, rhs)
-      | [< >] -> lhs) stream
+  | [< lhs = parse_summand;
+       rest = parser
+         | [< 'Token.Plus; rhs = parse_expr >] -> Ast.Plus(lhs, rhs)
+         | [< 'Token.Minus; rhs = parse_expr >] -> Ast.Minus(lhs, rhs)
+         | [< >] -> lhs >] -> rest
 
 and parse_summand = parser
-  | [< lhs = parse_multiplier; stream >] ->
-    (parser
-      | [< 'Token.Mul; rhs = parse_summand >] -> Ast.Mul(lhs, rhs)
-      | [< 'Token.Div; rhs = parse_summand >] -> Ast.Div(lhs, rhs)
-      | [< 'Token.Mod; rhs = parse_summand >] -> Ast.Mod(lhs, rhs)
-      | [< >] -> lhs) stream
+  | [< lhs = parse_multiplier;
+       rest = parser
+         | [< 'Token.Mul; rhs = parse_summand >] -> Ast.Mul(lhs, rhs)
+         | [< 'Token.Div; rhs = parse_summand >] -> Ast.Div(lhs, rhs)
+         | [< 'Token.Mod; rhs = parse_summand >] -> Ast.Mod(lhs, rhs)
+         | [< >] -> lhs >] -> rest
 
 and parse_multiplier = parser
   | [< 'Token.Number x >] -> Ast.Number x
+  | [< 'Token.Variable v >] -> Ast.Variable v
   | [< 'Token.Keyword '('; e = parse_expr; 'Token.Keyword ')' >] -> e
