d4464487d05d17e595f971d775951e81ae8df64b
Svyatoslav Gryaznov <nightuser@ya.ru>
2015-03-06 22:02:14 +0300

    Add comparison ops

--- main.ml
+++ main.ml
@@ -1,6 +1,6 @@
 let main () =
   let output =
-    let input      = "3 * 19 - 27 % 9 + 11 / 4" in
+    let input      = "3 * 19 - (17 + 11) % (3 - 1) + 11 / 4 == 11 - 3 > 7" in
     let lex_stream = Lexer.lex (Stream.of_string input) in
     let ast        = Parser.parse_expr lex_stream in
     Ast.pretty_print ast
--- parser.ml
+++ parser.ml
@@ -1,10 +1,26 @@
 (* dummy parser *)
 
 let rec parse_expr = parser
+  | [< lhs = parse_1;
+       rest = parser
+         | [< 'Token.Eq; rhs = parse_expr >] -> Ast.Eq(lhs, rhs)
+         | [< 'Token.NEq; rhs = parse_expr >] -> Ast.NEq(lhs, rhs)
+         | [< >] -> lhs >] -> rest
+
+and parse_1 = parser
+  | [< lhs = parse_2;
+       rest = parser
+         | [< 'Token.Lt; rhs = parse_1 >] -> Ast.Lt(lhs, rhs)
+         | [< 'Token.Le; rhs = parse_1 >] -> Ast.Le(lhs, rhs)
+         | [< 'Token.Gt; rhs = parse_1 >] -> Ast.Gt(lhs, rhs)
+         | [< 'Token.Ge; rhs = parse_1 >] -> Ast.Ge(lhs, rhs)
+         | [< >] -> lhs >] -> rest
+
+and parse_2 = parser
   | [< lhs = parse_summand;
        rest = parser
-         | [< 'Token.Plus; rhs = parse_expr >] -> Ast.Plus(lhs, rhs)
-         | [< 'Token.Minus; rhs = parse_expr >] -> Ast.Minus(lhs, rhs)
+         | [< 'Token.Plus; rhs = parse_2 >] -> Ast.Plus(lhs, rhs)
+         | [< 'Token.Minus; rhs = parse_2 >] -> Ast.Minus(lhs, rhs)
          | [< >] -> lhs >] -> rest
 
 and parse_summand = parser
