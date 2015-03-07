5520fc70d61e69d6c54b28691a392485462dadb8
nauru <menshov-sergej@yandex.ru>
2015-03-07 19:16:08 +0300

    toLambda

--- ast.ml
+++ ast.ml
@@ -1,7 +1,6 @@
 type expr =
   | Variable of string
   | Number of int
-  | Pow of expr * expr
   | Mul of expr * expr
   | Div of expr * expr
   | Mod of expr * expr
@@ -13,11 +12,16 @@ type expr =
   | Ge of expr * expr
   | Eq of expr * expr
   | NEq of expr * expr
+  | Assign of expr * expr
+
+
+
+
+
 
 let rec pretty_print = function
   | Variable v -> v
   | Number x -> string_of_int x
-  | Pow (x, y) -> "(^ " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
   | Mul (x, y) -> "(* " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
   | Div (x, y) -> "(/ " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
   | Mod (x, y) -> "(% " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
@@ -29,3 +33,25 @@ let rec pretty_print = function
   | Ge (x, y) -> "(>= " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
   | Eq (x, y) -> "(== " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
   | NEq (x, y) -> "(!= " ^ (pretty_print x) ^ " " ^ (pretty_print y) ^ ")"
+  | Assign (x, y) -> ((pretty_print x) ^ ":=" ^ pretty_print y)
+
+let rec numeral x = 
+  match x  with 
+  | 0 -> "z"
+  | _ -> "s(" ^ (numeral (x - 1) ) ^ ")"
+
+let rec toLambda = function
+  | Variable v -> v
+  | Number x -> "(lsz." ^ (numeral x) ^ ")"
+  | Mul (left, right) -> "((lmnsz.m (n s) z) " ^ toLambda left ^ " " ^ toLambda right ^ ")"
+  | Plus (left, right) -> "((lmnsz.m s (n s z)) " ^ toLambda left ^ " " ^ toLambda right ^ ")"
+  | Minus (left, right) -> "((lab.rec b pred a) " ^ toLambda left ^ " " ^ toLambda right ^ ")"
+  | Div (left, right) -> "((Y(lfnm.iif (le n m) (lsz.z) plus ((f (minus n m) m) 1))) " ^ toLambda left ^ toLambda right ^ ")"
+  | Mod (left, right) -> "((Y(lfnm.iif (gt n m) (f (minus n m) m)) n)" ^ toLambda left ^ toLambda right ^ ")"
+  | Eq (left, right) -> "((lab.and (isZero (minus a b)) (isZero (minus b a) ) tru fls )" ^ toLambda left ^ toLambda right ^ ")"
+  | NEq (left, right) -> "((lab.and (isZero (minus a b)) (isZero (minus b a) ) fls tru )" ^ toLambda left ^ toLambda right ^ ")"
+  | Lt (left, right) -> "((lab.NEq (minus b a) (lsz.z)" ^ toLambda left ^ toLambda right ^ ")"
+  | Gt (left, right) -> "((lab.NEq (minus a b) (lsz.z)" ^ toLambda left ^ toLambda right ^ ")"
+  | Le (left, right) -> "((lab.or (Lt a b) (Eq a b))" ^ toLambda left ^ toLambda right ^ ")"
+  | Ge (left, right) -> "((lab.or (Gt a b) (Eq a b))" ^ toLambda left ^ toLambda right ^ ")"
+  | _ -> "ToDo"
--- main.ml
+++ main.ml
@@ -2,7 +2,7 @@ let main () =
   let output =
     let tokens = Lexer.lex (Stream.of_channel stdin) in
     let ast    = Parser.parse_expr tokens in
-    Ast.pretty_print ast
+    Ast.toLambda ast
   in
   print_endline output
 ;;
